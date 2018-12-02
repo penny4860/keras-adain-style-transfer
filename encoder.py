91e1f2455d49a5d839d4d052c1be3b7fc98db247
jeongjoonsup <penny4860@gmail.com>
2018-12-02 21:32:55 +0900

    vgg encoder output confirmed

--- encoder.py
+++ encoder.py
@@ -80,29 +80,43 @@ def vgg19(t7_file, input_shape=[224,224,3]):
         x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
         
         # Block 2
-        x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
-        x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+        x = SpatialReflectionPadding()(x)
+        x = Conv2D(128, (3, 3), activation='relu', padding='valid', name='block2_conv1')(x)
+        x = SpatialReflectionPadding()(x)
+        x = Conv2D(128, (3, 3), activation='relu', padding='valid', name='block2_conv2')(x)
         x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
         
         # Block 3
-        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
-        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
-        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
-        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv4')(x)
+        x = SpatialReflectionPadding()(x)
+        x = Conv2D(256, (3, 3), activation='relu', padding='valid', name='block3_conv1')(x)
+        x = SpatialReflectionPadding()(x)
+        x = Conv2D(256, (3, 3), activation='relu', padding='valid', name='block3_conv2')(x)
+        x = SpatialReflectionPadding()(x)
+        x = Conv2D(256, (3, 3), activation='relu', padding='valid', name='block3_conv3')(x)
+        x = SpatialReflectionPadding()(x)
+        x = Conv2D(256, (3, 3), activation='relu', padding='valid', name='block3_conv4')(x)
         x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
         
         # Block 4
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv4')(x)
+        x = SpatialReflectionPadding()(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='valid', name='block4_conv1')(x)
+        x = SpatialReflectionPadding()(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='valid', name='block4_conv2')(x)
+        x = SpatialReflectionPadding()(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='valid', name='block4_conv3')(x)
+        x = SpatialReflectionPadding()(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='valid', name='block4_conv4')(x)
         x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
         
         # Block 5
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv4')(x)
+        x = SpatialReflectionPadding()(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='valid', name='block5_conv1')(x)
+        x = SpatialReflectionPadding()(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='valid', name='block5_conv2')(x)
+        x = SpatialReflectionPadding()(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='valid', name='block5_conv3')(x)
+        x = SpatialReflectionPadding()(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='valid', name='block5_conv4')(x)
         model = Model(img_input, x, name='vgg19')
         return model
     
@@ -136,8 +150,8 @@ if __name__ == '__main__':
         model = tf.keras.models.Model(vgg.input, vgg.layers[layer_idx].output)
         return model.predict(images_keras)
 
-    images_torch = run_from_torch(content, layer_idx=7, resize=[32,32])
-    images_keras = run_from_keras(content, layer_idx=5, resize=[32,32])
+    images_torch = run_from_torch(content, layer_idx=-1, resize=[224,224])
+    images_keras = run_from_keras(content, layer_idx=-1, resize=[224,224])
     
     print(images_torch.shape, images_keras.shape)
     print("difference = ", calc_diff(images_torch, images_keras))
