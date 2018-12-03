bdf3691d9cf12a609ff8bb0629dbf271397146c6
jeongjoonsup <penny4860@gmail.com>
2018-12-03 19:39:48 +0900

    get block4_1 layer output

--- encoder.py
+++ encoder.py
@@ -147,9 +147,8 @@ if __name__ == '__main__':
         model = tf.keras.models.Model(vgg.input, vgg.layers[layer_idx].output)
         return model.predict(images_keras)
 
-    images_torch = run_from_torch(content, layer_idx=-1, resize=[224,224])
-    images_keras = run_from_keras(content, layer_idx=-1, resize=[224,224])
-    
+    images_torch = run_from_torch(content, layer_idx=30, resize=[224,224])
+    images_keras = run_from_keras(content, layer_idx=-16, resize=[224,224])
     print(images_torch.shape, images_keras.shape)
     print("difference = ", calc_diff(images_torch, images_keras))
 
