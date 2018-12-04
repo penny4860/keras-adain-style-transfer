610f9fc917eeea71b1977f1f782fd6be2cada6a6
jeongjoonsup <penny4860@gmail.com>
2018-12-05 02:32:39 +0900

    postprocess() extracted

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from adain import PROJECT_ROOT
 from adain.models import adain_style_transfer
 from adain.encoder import load_and_preprocess_img
 
+import numpy as np
+
+def postprocess(images):
+    # scale range
+    images = images * 256
+    # bgr -> rgb
+    images = images[:,:,:,::-1]
+    return images.astype(np.uint8)[0]
+    
+
 if __name__ == '__main__':
     content = os.path.join(PROJECT_ROOT, 'input/content/modern.jpg')
     style = os.path.join(PROJECT_ROOT, 'input/style/goeritz.jpg')
@@ -14,17 +24,15 @@ if __name__ == '__main__':
     content_imgs = load_and_preprocess_img(content, (512,512))
     style_imgs = load_and_preprocess_img(style, (512,512))
     stylized_imgs = model.predict([content_imgs, style_imgs])
-    stylized_imgs = stylized_imgs * 256
-    stylized_imgs = stylized_imgs[:,:,:,::-1]
+    stylized_img = postprocess(stylized_imgs)
 
     import matplotlib.pyplot as plt
-    import numpy as np
     fig, ax = plt.subplots()
 #     plt.subplot(1, 3, 1)
-#     plt.imshow(content_imgs[0])
+#     plt.imshow(content_imgs)
 #     plt.subplot(1, 3, 2)
-#     plt.imshow(style_imgs[0])
+#     plt.imshow(style_imgs)
     plt.subplot(1, 3, 3)
-    plt.imshow(stylized_imgs[0].astype(np.uint8))
+    plt.imshow(stylized_img)
     plt.show()
 
