88cf94fd9600ed163ef282c8ce006c8e303fc7a1
jeongjoonsup <penny4860@gmail.com>
2019-02-03 17:16:45 +0900

    get_params() added

--- adain/utils.py
+++ adain/utils.py
@@ -23,6 +23,23 @@ def postprocess(images):
     return images[0].astype(np.uint8)
 
 
+def get_params(t7_file):
+    import torchfile
+    t7 = torchfile.load(t7_file, force_8bytes_long=True)
+    weights = []
+    biases = []
+    for idx, module in enumerate(t7.modules):
+        weight = module.weight
+        bias = module.bias
+        if idx == 0:
+            print(bias)
+        elif weight is not None:
+            weight = weight.transpose([2,3,1,0])
+            weights.append(weight)
+            biases.append(bias)
+    return weights, biases
+
+
 def set_params(model, weights, biases):
     i = 0
     for layer in model.layers:
