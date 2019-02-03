775d2160f424873dc97695bb5b1bee67f11e9432
jeongjoonsup <penny4860@gmail.com>
2019-02-04 02:10:46 +0900

    SpatialReflectionPadding layer added

--- adain/decoder.py
+++ adain/decoder.py
@@ -5,6 +5,7 @@ import keras
 import os
 
 from adain import PROJECT_ROOT
+from adain.encoder import SpatialReflectionPadding
 
 t7_file = os.path.join(PROJECT_ROOT, "pretrained/decoder-content-similar.t7")
 
@@ -33,24 +34,33 @@ def combine_and_decode_model(input_shape=[None,None,512], alpha=1.0, t7_file=t7_
     x = AdaIN(alpha)([c_feat_input, s_feat_input])
 
     # Block 4
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block4_conv1_decode')(x)
+    x = SpatialReflectionPadding()(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='valid', name='block4_conv1_decode')(x)
     x = UpSampling2D()(x)
 
     # Block 3
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1_decode')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2_decode')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3_decode')(x)
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block3_conv4_decode')(x)
+    x = SpatialReflectionPadding()(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='valid', name='block3_conv1_decode')(x)
+    x = SpatialReflectionPadding()(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='valid', name='block3_conv2_decode')(x)
+    x = SpatialReflectionPadding()(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='valid', name='block3_conv3_decode')(x)
+    x = SpatialReflectionPadding()(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='valid', name='block3_conv4_decode')(x)
     x = UpSampling2D()(x)
 
     # Block 2
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1_decode')(x)
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block2_conv2_decode')(x)
+    x = SpatialReflectionPadding()(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='valid', name='block2_conv1_decode')(x)
+    x = SpatialReflectionPadding()(x)
+    x = Conv2D(64, (3, 3), activation='relu', padding='valid', name='block2_conv2_decode')(x)
     x = UpSampling2D()(x)
 
     # Block 1
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1_decode')(x)
-    x = Conv2D(3, (3, 3), activation=None, padding='same', name='block1_conv2_decode')(x)
+    x = SpatialReflectionPadding()(x)
+    x = Conv2D(64, (3, 3), activation='relu', padding='valid', name='block1_conv1_decode')(x)
+    x = SpatialReflectionPadding()(x)
+    x = Conv2D(3, (3, 3), activation=None, padding='valid', name='block1_conv2_decode')(x)
     x = PostPreprocess(name="output")(x)
     
     model = Model([c_feat_input, s_feat_input], x, name='decoder')
