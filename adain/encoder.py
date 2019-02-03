a04f2090016b2945b171e0ced9e0d3cb1ad0415a
jeongjoonsup <penny4860@gmail.com>
2019-02-03 23:45:16 +0900

    keras -> tf.keras

--- adain/encoder.py
+++ adain/encoder.py
@@ -5,7 +5,6 @@ from adain import PROJECT_ROOT
 from adain.utils import get_params, set_params
 
 import tensorflow as tf
-import keras
 
 
 vgg_t7_file = os.path.join(PROJECT_ROOT, "pretrained", 'vgg_normalised.t7')
@@ -14,11 +13,11 @@ vgg_t7_file = os.path.join(PROJECT_ROOT, "pretrained", 'vgg_normalised.t7')
 def vgg_encoder():
     vgg = vgg19(vgg_t7_file, [512,512,3])
     # Todo : hard-coding
-    model = keras.models.Model(vgg.input, vgg.layers[-9].output)
+    model = tf.keras.models.Model(vgg.input, vgg.layers[-9].output)
     return model
     
     
-class VggPreprocess(keras.layers.Layer):
+class VggPreprocess(tf.keras.layers.Layer):
 
     def __init__(self, **kwargs):
         super(VggPreprocess, self).__init__(**kwargs)
@@ -34,10 +33,10 @@ def vgg19(t7_file=vgg_t7_file, input_shape=[256,256,3]):
     
     def _build_model(input_shape):
 
-        Input = keras.layers.Input
-        Conv2D = keras.layers.Conv2D
-        MaxPooling2D = keras.layers.MaxPooling2D
-        Model = keras.models.Model
+        Input = tf.keras.layers.Input
+        Conv2D = tf.keras.layers.Conv2D
+        MaxPooling2D = tf.keras.layers.MaxPooling2D
+        Model = tf.keras.models.Model
         
         x = Input(shape=input_shape, name="input")
         img_input = x
@@ -87,7 +86,7 @@ if __name__ == '__main__':
 
     # 1. to frozen pb
     from adain.utils import freeze_session
-    from keras import backend as K
+    K = tf.keras.backend
     frozen_graph = freeze_session(K.get_session(),
                                   output_names=[out.op.name for out in encoder_model.outputs])
     tf.train.write_graph(frozen_graph, "models", "encoder.pb", as_text=False)
