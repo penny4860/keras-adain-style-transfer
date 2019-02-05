65f62385e844d46ccf4be440fb98e6b0a66bc513
jeongjoonsup <penny4860@gmail.com>
2019-02-05 18:17:27 +0900

    vgg19_light 모델

--- adain/encoder.py
+++ adain/encoder.py
@@ -89,62 +89,82 @@ def vgg19(t7_file=vgg_t7_file, input_shape=[256,256,3]):
     return model
 
 
-def vgg19_depthwise(input_shape=[256,256,3]):
+def vgg19_light(input_shape=[256,256,3]):
 
     Input = tf.keras.layers.Input
     Conv2D = tf.keras.layers.Conv2D
-    MaxPooling2D = tf.keras.layers.MaxPooling2D
     DepthwiseConv2D = tf.keras.layers.DepthwiseConv2D
     Model = tf.keras.models.Model
+    BatchNormalization = tf.keras.layers.BatchNormalization
+    Activateion = tf.keras.layers.Activation
     
     x = Input(shape=input_shape, name="input")
     img_input = x
 
     # Block 1
     x = VggPreprocess()(x)
-    x = SpatialReflectionPadding()(x)
-    x = DepthwiseConv2D((3, 3), activation='relu', padding='valid')(x)
-    x = Conv2D(64, (1, 1), activation='relu', padding='valid', name='block1_conv1')(x)
-    x = SpatialReflectionPadding()(x)
-    x = DepthwiseConv2D((3, 3), activation='relu', padding='valid')(x)
-    x = Conv2D(64, (1, 1), activation='relu', padding='valid', name='block1_conv2')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+    x = Conv2D(32, (3, 3), strides=2, use_bias=False, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    # (112,112,32)
+
+    x = DepthwiseConv2D((3, 3), use_bias=False, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    x = Conv2D(64, (1, 1), use_bias=False, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    # (112,112,64)
+
+    x = DepthwiseConv2D((3, 3), strides=2, use_bias=False, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    # (56,56,64)
+    x = Conv2D(128, (1, 1), strides=1, use_bias=False, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    # (56,56,128)
+
+    x = DepthwiseConv2D((3, 3), strides=1, use_bias=False, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    x = Conv2D(128, (1, 1), strides=1, use_bias=False, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    # (56,56,128)
+
+    x = DepthwiseConv2D((3, 3), strides=2, use_bias=False, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    # (28,28,128)
+    x = Conv2D(256, (1, 1), strides=1, use_bias=False, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    # (28,28,256)
+
+    x = DepthwiseConv2D((3, 3), strides=1, use_bias=False, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    x = Conv2D(256, (1, 1), strides=1, use_bias=False, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    # (28,28,256)
+
+    x = DepthwiseConv2D((3, 3), strides=1, use_bias=False, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    x = Conv2D(512, (1, 1), strides=1, use_bias=False, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu", name="output")(x)
+    # (28,28,512)
     
-    # Block 2
-    x = SpatialReflectionPadding()(x)
-    x = DepthwiseConv2D((3, 3), activation='relu', padding='valid')(x)
-    x = Conv2D(128, (1, 1), activation='relu', padding='valid', name='block2_conv1')(x)
-    x = SpatialReflectionPadding()(x)
-    x = DepthwiseConv2D((3, 3), activation='relu', padding='valid')(x)
-    x = Conv2D(128, (1, 1), activation='relu', padding='valid', name='block2_conv2')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
-    
-    # Block 3
-    x = SpatialReflectionPadding()(x)
-    x = DepthwiseConv2D((3, 3), activation='relu', padding='valid')(x)
-    x = Conv2D(256, (1, 1), activation='relu', padding='valid', name='block3_conv1')(x)
-    x = SpatialReflectionPadding()(x)
-    x = DepthwiseConv2D((3, 3), activation='relu', padding='valid')(x)
-    x = Conv2D(256, (1, 1), activation='relu', padding='valid', name='block3_conv2')(x)
-    x = SpatialReflectionPadding()(x)
-    x = DepthwiseConv2D((3, 3), activation='relu', padding='valid')(x)
-    x = Conv2D(256, (1, 1), activation='relu', padding='valid', name='block3_conv3')(x)
-    x = SpatialReflectionPadding()(x)
-    x = DepthwiseConv2D((3, 3), activation='relu', padding='valid')(x)
-    x = Conv2D(256, (1, 1), activation='relu', padding='valid', name='block3_conv4')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
-    
-    # Block 4
-    x = SpatialReflectionPadding()(x)
-    x = DepthwiseConv2D((3, 3), activation='relu', padding='valid')(x)
-    x = Conv2D(512, (1, 1), activation='relu', padding='valid', name="output")(x)
-    model = Model(img_input, x, name='vgg19_depthwise')
+    model = Model(img_input, x, name='vgg19_light')
     return model
 
 
 if __name__ == '__main__':
     model = vgg19()
-    light_model = vgg19_depthwise()
+    light_model = vgg19_light()
     
     model.summary()
     light_model.summary()
