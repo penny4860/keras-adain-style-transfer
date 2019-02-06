80f3b7146a8465cdca7874c3ea8fac88b4d6ea23
jeongjoonsup <penny4860@gmail.com>
2019-02-06 14:35:01 +0900

    2-samples 로 overfitting 실험

--- experiments/train_encoder.py
+++ experiments/train_encoder.py
@@ -52,23 +52,25 @@ def _create_callbacks(saved_weights_name="mobile_encoder.h5"):
 if __name__ == '__main__':
     # tf.keras.backend.set_session(session)
     model = vgg19_light(input_shape=[256,256,3])
-    model.load_weights("mobile_init.h5")
+    model.load_weights("mobile_encoder.h5", by_name=True)
+#     for l in model.layers[:-6]:
+#         l.trainable = False
 
 #     light_model.summary()
     import glob
     fnames = glob.glob(IMG_ROOT+"/*.jpg")
-    train_generator = BatchGenerator(fnames[:160], batch_size=4, shuffle=True)
+    train_generator = BatchGenerator(fnames[:2], batch_size=2, shuffle=False)
     # valid_generator = BatchGenerator(fnames[160:], batch_size=4, shuffle=False)
     
     # 2. create loss function
     model.compile(loss="mean_squared_error",
-                  optimizer=tf.keras.optimizers.Adam(lr=1e-4))
+                  optimizer=tf.keras.optimizers.Adam(lr=1e-2))
     model.fit_generator(train_generator,
                         steps_per_epoch=len(train_generator),
                         callbacks=_create_callbacks(),
                         validation_data  = train_generator,
                         validation_steps = len(train_generator),
-                        epochs=10)
+                        epochs=1000)
 
 
 
