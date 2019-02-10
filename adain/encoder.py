6de4d4def7543efbf4c0720dd70b58e3eb016cbc
jeongjoonsup <penny4860@gmail.com>
2019-02-10 18:24:40 +0900

    encoder model architecture changed

--- adain/encoder.py
+++ adain/encoder.py
@@ -99,106 +99,85 @@ def vgg19_light(input_shape=[256,256,3]):
     Model = keras.models.Model
     BatchNormalization = keras.layers.BatchNormalization
     Activateion = keras.layers.Activation
-    
+    MaxPooling2D = keras.layers.MaxPooling2D
+
     x = Input(shape=input_shape, name="input")
     img_input = x
 
     # Block 1
     x = VggPreprocess()(x)
-    x = Conv2D(32, (3, 3), strides=2, use_bias=False, padding='same')(x)
+    x = Conv2D(64, (3, 3), padding='same', name='block1_conv1')(x)
     x = BatchNormalization()(x)
     x = Activateion("relu")(x)
-    # (112,112,32)
+    
+    x = DepthwiseConv2D((3, 3), padding='same', name='block1_conv2')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    x = Conv2D(64, (1, 1), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+    
+    # Block 2
+    x = DepthwiseConv2D((3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    x = Conv2D(128, (1, 1), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    x = DepthwiseConv2D((3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    x = Conv2D(128, (1, 1), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activateion("relu")(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
 
-    x = DepthwiseConv2D((3, 3), use_bias=False, padding='same')(x)
+    # Block 3
+    x = DepthwiseConv2D((3, 3), padding='same')(x)
     x = BatchNormalization()(x)
     x = Activateion("relu")(x)
-    x = Conv2D(64, (1, 1), use_bias=False, padding='same')(x)
+    x = Conv2D(256, (1, 1), padding='same')(x)
     x = BatchNormalization()(x)
     x = Activateion("relu")(x)
-    # (112,112,64)
 
-    x = DepthwiseConv2D((3, 3), strides=2, use_bias=False, padding='same')(x)
+    x = DepthwiseConv2D((3, 3), padding='same')(x)
     x = BatchNormalization()(x)
     x = Activateion("relu")(x)
-    # (56,56,64)
-    x = Conv2D(128, (1, 1), strides=1, use_bias=False, padding='same')(x)
+    x = Conv2D(256, (1, 1), padding='same')(x)
     x = BatchNormalization()(x)
     x = Activateion("relu")(x)
-    # (56,56,128)
 
-    x = DepthwiseConv2D((3, 3), strides=1, use_bias=False, padding='same')(x)
+    x = DepthwiseConv2D((3, 3), padding='same')(x)
     x = BatchNormalization()(x)
     x = Activateion("relu")(x)
-    x = Conv2D(128, (1, 1), strides=1, use_bias=False, padding='same')(x)
+    x = Conv2D(256, (1, 1), padding='same')(x)
     x = BatchNormalization()(x)
     x = Activateion("relu")(x)
-    # (56,56,128)
 
-    x = DepthwiseConv2D((3, 3), strides=2, use_bias=False, padding='same')(x)
+    x = DepthwiseConv2D((3, 3), padding='same')(x)
     x = BatchNormalization()(x)
     x = Activateion("relu")(x)
-    # (28,28,128)
-    x = Conv2D(256, (1, 1), strides=1, use_bias=False, padding='same')(x)
+    x = Conv2D(256, (1, 1), padding='same')(x)
     x = BatchNormalization()(x)
     x = Activateion("relu")(x)
-    # (28,28,256)
-
-    x = DepthwiseConv2D((3, 3), strides=1, use_bias=False, padding='same')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+    
+    # Block 4
+    x = DepthwiseConv2D((3, 3), padding='same')(x)
     x = BatchNormalization()(x)
     x = Activateion("relu")(x)
-    x = Conv2D(256, (1, 1), strides=1, use_bias=False, padding='same')(x)
+    x = Conv2D(512, (1, 1), padding='same')(x)
     x = BatchNormalization()(x)
     x = Activateion("relu")(x)
-    # (28,28,256)
-
-    x = DepthwiseConv2D((3, 3), strides=1, use_bias=False, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Activateion("relu")(x)
-    x = Conv2D(512, (1, 1), strides=1, use_bias=False, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Activateion("relu")(x)
-    # (28,28,512)
-
-    x = DepthwiseConv2D((3, 3), strides=1, use_bias=False, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Activateion("relu")(x)
-    x = Conv2D(512, (1, 1), strides=1, use_bias=False, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Activateion("relu", name="output")(x)
-
     model = Model(img_input, x, name='vgg19_light')
     return model
 
 
 if __name__ == '__main__':
-    model = vgg19()
+    model = vgg19(None)
     light_model = vgg19_light()
-    mobilenet = keras.applications.mobilenet.MobileNet(input_shape=(224,224,3))
-    print("======================================================")
-    conv_params = []
-    bn_params = []
-    for layer in mobilenet.layers:
-        params = layer.get_weights()
-        if len(params) == 1:
-            conv_params.append(params)
-        if len(params) == 4:
-            bn_params.append(params)
-    print("======================================================")
 
-    ci = 0
-    bi = 0    
-    for layer in light_model.layers:
-        params = layer.get_weights()
-        if len(params) == 1:
-            print(layer.name, params[0].shape, conv_params[ci][0].shape)
-            layer.set_weights(conv_params[ci])
-            ci += 1
-        if len(params) == 4:
-            print(layer.name, params[0].shape, bn_params[bi][0].shape)
-            layer.set_weights(bn_params[bi])
-            bi += 1
-    print(ci, bi)
-    light_model.save_weights("mobile_init.h5")
-    
+    model.summary()
+    light_model.summary()
 
