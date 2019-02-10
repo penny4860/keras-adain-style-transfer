6b6029222c31f08c7a4d8cd666d4313d2386e07a
jeongjoonsup <penny4860@gmail.com>
2019-02-10 19:20:13 +0900

    driver 구현 중

---
+++ experiments/extract_frozen_pb.py
@@ -0,0 +1,21 @@
+
+import tensorflow as tf
+
+if __name__ == '__main__':
+    from adain.encoder import vgg19_light
+    # encoder_model = vgg_encoder()
+    model = vgg19_light()
+    model.load_weights("../mobile_encoder.h5")
+    model.summary()
+
+    # 1. to frozen pb
+    from adain.utils import freeze_session
+    K = tf.keras.backend
+    frozen_graph = freeze_session(K.get_session(),
+                                  output_names=[out.op.name for out in model.outputs])
+    tf.train.write_graph(frozen_graph, "models", "encoder.pb", as_text=False)
+    # input_c,input_s  / output/mul
+    for t in model.inputs + model.outputs:
+        print("op name: {}, shape: {}".format(t.op.name, t.shape))
+
+
