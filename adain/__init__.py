de6f19f9938a01516f27ed867a6ce530c9b3fc06
jeongjoonsup <penny4860@gmail.com>
2019-02-10 21:51:44 +0900

    main driver cleaning

--- adain/__init__.py
+++ adain/__init__.py
@@ -3,4 +3,6 @@ import os
 
 PKG_ROOT = os.path.dirname(__file__)
 PROJECT_ROOT = os.path.dirname(PKG_ROOT)
+MODEL_ROOT = os.path.join(PKG_ROOT, "models")
+
 
--- main.py
+++ main.py
@@ -4,12 +4,30 @@ import numpy as np
 import cv2
 import argparse
 import matplotlib.pyplot as plt
+import os
 
 from adain.utils import preprocess
 from adain.graph import load_graph_from_pb
+from adain import MODEL_ROOT
+
+DEFAULT_ENCODER_PB = os.path.join(MODEL_ROOT, "encoder_opt.pb")
+DEFAULT_DECODER_PB = os.path.join(MODEL_ROOT, "decoder_opt.pb")
+
 
 argparser = argparse.ArgumentParser(
     description='style transfer with Adaptive Instance Normalization')
+
+argparser.add_argument(
+    '-e',
+    '--encoder_pb',
+    default=DEFAULT_ENCODER_PB,
+    help='encoder pb file')
+
+argparser.add_argument(
+    '-d',
+    '--decoder_pb',
+    default=DEFAULT_DECODER_PB,
+    help='decoder pb file')
 
 argparser.add_argument(
     '-c',
@@ -48,21 +66,21 @@ if __name__ == '__main__':
     s_img_prep = preprocess(s_img, (256,256))
     
     # 3. encoding
-    sess = load_graph_from_pb("adain/models/encoder_opt.pb")
+    sess = load_graph_from_pb(args.encoder_pb)
     tensor_input = sess.graph.get_tensor_by_name('import/input:0')
     tensor_output = sess.graph.get_tensor_by_name('import/output/Relu:0')
     c_feat = sess.run(tensor_output, {tensor_input: c_img_prep})
     s_feat = sess.run(tensor_output, {tensor_input: s_img_prep})
 
     # 4. mix & decoding
-    sess = load_graph_from_pb("adain/models/decoder_opt.pb")
+    sess = load_graph_from_pb(args.decoder_pb)
     tensor_input_c = sess.graph.get_tensor_by_name('import_1/input_c:0')
     tensor_input_s = sess.graph.get_tensor_by_name('import_1/input_s:0')
     tensor_output = sess.graph.get_tensor_by_name('import_1/output/mul:0')
     stylized_imgs = sess.run(tensor_output, {tensor_input_c: c_feat, tensor_input_s: s_feat})
     stylized_img = stylized_imgs[0].astype(np.uint8)
    
-    # 4. plot
+    # 5. plot
     fig, ax = plt.subplots()
     plt.subplot(1, 3, 1)
     plt.axis('off')
