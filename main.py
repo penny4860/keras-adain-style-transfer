906926d63cbe98c6b39591592944fbde72140b86
jeongjoonsup <penny4860@gmail.com>
2019-02-10 18:59:47 +0900

    mobile encoder 로 stylized image 리컨 성공

--- main.py
+++ main.py
@@ -60,19 +60,36 @@ if __name__ == '__main__':
     s_img_prep = preprocess(s_img, (256,256))
     
     # 3. encoding
-    sess = load_graph("adain/models/encoder_opt.pb")
-    tensor_input = sess.graph.get_tensor_by_name('import/input:0')
-    tensor_output = sess.graph.get_tensor_by_name('import/output/Relu:0')
-    c_feat = sess.run(tensor_output, {tensor_input: c_img_prep})
-    s_feat = sess.run(tensor_output, {tensor_input: s_img_prep})
-
-    # 4. mix & decoding
-    sess = load_graph("adain/models/decoder_opt.pb")
-    tensor_input_c = sess.graph.get_tensor_by_name('import_1/input_c:0')
-    tensor_input_s = sess.graph.get_tensor_by_name('import_1/input_s:0')
-    tensor_output = sess.graph.get_tensor_by_name('import_1/output/mul:0')
-    stylized_imgs = sess.run(tensor_output, {tensor_input_c: c_feat, tensor_input_s: s_feat})
+    from adain.encoder import vgg_encoder, vgg19_light
+    # encoder_model = vgg_encoder()
+    encoder_model = vgg19_light()
+    encoder_model.load_weights("mobile_encoder.h5")
+    
+    c_features = encoder_model.predict(c_img_prep)
+    s_features = encoder_model.predict(s_img_prep)
+    print(c_features.shape, s_features.shape)
+     
+    # 2) combine & decode
+    from adain.decoder import combine_and_decode_model
+    decoder_model = combine_and_decode_model(alpha=1.0)
+    stylized_imgs = decoder_model.predict([c_features, s_features])
+    print(stylized_imgs.shape)
     stylized_img = stylized_imgs[0].astype(np.uint8)
+
+    
+#     sess = load_graph("adain/models/encoder_opt.pb")
+#     tensor_input = sess.graph.get_tensor_by_name('import/input:0')
+#     tensor_output = sess.graph.get_tensor_by_name('import/output/Relu:0')
+#     c_feat = sess.run(tensor_output, {tensor_input: c_img_prep})
+#     s_feat = sess.run(tensor_output, {tensor_input: s_img_prep})
+# 
+#     # 4. mix & decoding
+#     sess = load_graph("adain/models/decoder_opt.pb")
+#     tensor_input_c = sess.graph.get_tensor_by_name('import_1/input_c:0')
+#     tensor_input_s = sess.graph.get_tensor_by_name('import_1/input_s:0')
+#     tensor_output = sess.graph.get_tensor_by_name('import_1/output/mul:0')
+#     stylized_imgs = sess.run(tensor_output, {tensor_input_c: c_feat, tensor_input_s: s_feat})
+#     stylized_img = stylized_imgs[0].astype(np.uint8)
    
     # 4. plot
     fig, ax = plt.subplots()
